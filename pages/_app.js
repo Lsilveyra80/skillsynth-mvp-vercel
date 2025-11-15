4f5c6d1a70b7ba5967d6d2c70b4769e529c94e91
Luigi1980 <47399028+Lsilveyra80@users.noreply.github.com>
2025-11-15 19:24:06 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import '../styles/globals.css';
+import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics 4 */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-QJ4XF5HF0X"
+        strategy="afterInteractive"
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-QJ4XF5HF0X', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
+      {/* Tu app */}
+      <Component {...pageProps} />
+    </>
+  );
 }
