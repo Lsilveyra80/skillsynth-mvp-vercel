ba7d29e0f493342a76df0b1360e4d5cc3261188a
Luigi1980 <47399028+Lsilveyra80@users.noreply.github.com>
2025-11-17 11:01:43 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,7 @@
-import "@/styles/globals.css";
-import Script from "next/script";
-import { Analytics } from '@vercel/analytics/next';
+// pages/_app.js
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      {/* Google Analytics 4 */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-QJ4XF5HF0X"
-        strategy="afterInteractive"
-      />
-      <Script id="ga4-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-QJ4XF5HF0X', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
-
-      {/* Tu aplicación */}
-      <Component {...pageProps} />
-
-      {/* Vercel Analytics */}
-      <Analytics />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
+
